e8ed16e745207f55f2e5e834f85355927017b72a
Kav-Senpai <106959520+Kav-Senpai@users.noreply.github.com>
2025-03-27 22:38:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, styled } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Authentication from "./pages/Authentication";
+import { useState } from "react";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
 
 function App() {
+  const [user, setUser] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+      {user ? (
+        <Container>
+          <Navbar />
+        </Container>
+          ) : (
+            <Container>
+              <Authentication/>
+            </Container>
+          )}
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
